1ec96c1680799e26cc8d010dfeb7c089cedaa8fe
Nei Designer <nei.jr.2013@gmail.com>
2019-10-03 07:39:05 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://localhost:3333" || "https://omnistackheroku.herokuapp.com",
+    baseURL: "https://omnistackheroku.herokuapp.com",
 });
 
 export default api;
